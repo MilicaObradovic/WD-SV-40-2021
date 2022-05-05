785a5cdbb6c232ec6b99ada907b6dab8daae6596
Milica Obradović <ns.o.milica@gmail.com>
2022-05-05 12:25:47 +0200

    initial commit

---
+++ funkcije.js
@@ -0,0 +1,33 @@
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+    document.getElementById("dugme").style.display = "none";
+    document.getElementById("dugme2").style.display = "none";
+    document.getElementById("myForm3").style.display = "none";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+    document.getElementById("dugme").style.display = "block";
+    document.getElementById("dugme2").style.display = "block";
+}
+
+function openForm2() {
+    document.getElementById("myForm2").style.display = "block";
+    document.getElementById("dugme").style.display = "none";
+    document.getElementById("dugme2").style.display = "none";
+    document.getElementById("myForm3").style.display = "none";
+}
+
+function closeForm2() {
+    document.getElementById("myForm2").style.display = "none";
+    document.getElementById("dugme").style.display = "block";
+    document.getElementById("dugme2").style.display = "block";
+}
+
+function openForm3() {
+    document.getElementById("myForm3").style.display = "block";
+}
+
+function closeForm3() {
+    document.getElementById("myForm3").style.display = "none";
+}
